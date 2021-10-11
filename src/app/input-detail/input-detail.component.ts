f7e528109bf887aeb07bbe0a1bf6d79cd796f89d
job karani <job.nyambura@student.moringaschool.com>
2021-10-11 15:42:42 +0300

    creates a quotes array to hold 2 default quotes

---
+++ src/app/input-detail/input-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit ,  } from '@angular/core';
+import { Quotes } from '../quotes';
+
+
+@Component({
+  selector: 'app-input-detail',
+  templateUrl: './input-detail.component.html',
+})
+export class InputDetailComponent implements OnInit {
+
+    quotes = [
+    new Quotes(0, 'Job', 'A large chair does not make a King.', new Date(), 0, 0 ),
+    new Quotes(0, 'Ayub', 'We must all either wear out or rust out,every one of us.My choice is to wear out.', new Date(), 0, 0 ),
+
+  ];
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
